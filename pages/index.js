fc282169914e4e97d1a7c73a01a1d1b40537b8a5
Muktar <bellom962@gmail.com>
2021-12-11 18:40:48 +0200

    Debug

--- pages/index.js
+++ pages/index.js
@@ -44,12 +44,14 @@ export default function Home({trendingResults,followResults,providers}) {
 
 export async function getServerSideProps(context)
 {
-  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
-    (res) => res.json()
-  );
-  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
-    (res) => res.json()
-  );
+  // const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+  //   (res) => res.json()
+  // );
+  // const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+  //   (res) => res.json()
+  // );
+  const trendingResults =[];
+  const followResults =[];
   const providers = await getProviders();
   const session = await getSession(context);
 
